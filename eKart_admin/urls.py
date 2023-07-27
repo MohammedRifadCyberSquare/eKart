bb0507ff1922deeb910797105f9e496e2a6c62ca
MohammedRifadCyberSuare <rifad@cybersquare.com>
2023-07-27 11:24:33 +0530

    admin_login added

--- eKart_admin/urls.py
+++ eKart_admin/urls.py
@@ -6,6 +6,7 @@ app_name = "ekart_admin"
 urlpatterns = [   
    path('',views.admin_home,name="admin_home"),
    path('category/add',views.add_category,name="add_category"),
+   path('login',views.admin_login,name="admin_login"),
    path('category/list',views.view_category,name="view_category"),
    path('sellers/list/pending',views.pending_sellers,name="pending_sellers"),
    path('sellers/list/approved',views.approved_sellers,name="approved_sellers"),
--- eKart_admin/views.py
+++ eKart_admin/views.py
@@ -1,6 +1,23 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from .models import EkartAdmin
 # Create your views here.
+
+def admin_login(request):
+    message = ''
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        try:
+            admin = EkartAdmin.objects.get(user_name = username, password = password)
+            return redirect('ekart_admin:admin_home')
+        except Exception as e:
+            print(e)
+            message = 'Invalid Username Or Password'
+
+
+    return render(request,'ekart_admin/admin_login.html', {'message': message,})
+
 def admin_home(request):
     return render(request,'ekart_admin/admin_home.html')
 
@@ -8,6 +25,12 @@ def view_category(request):
     return render(request,'ekart_admin/view_category.html')
 
 def add_category(request):
+    if request.method == 'POST':
+        category = request.POST['category']
+        description = request.POST['description']
+        cover_pic = request.FILES['cover_pic']
+
+        category
     return render(request,'ekart_admin/add_category.html')
 
 def pending_sellers(request):
